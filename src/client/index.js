03d4385c450d5ccf14158faa51fc080c61b070d6
Thomas Gilmore <tomtitans@aol.com>
2019-11-23 07:31:48 -0500

    refactor: Working on Project

--- src/client/index.js
+++ src/client/index.js
@@ -9,8 +9,8 @@ import './styles/header.scss'
 
 console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+//alert("I EXIST")
+//console.log("CHANGE!!");
 
 export {
     checkForName,
--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ var aylien = require("aylien_textapi");
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+var cors = require('cors')
 const app = express();
 
 /*var textapi = new aylien({
@@ -13,18 +13,24 @@ const app = express();
 });
 */
 
+app.use(cors())
+
+app.use(express.static('dist'))
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
-
-app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
+
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
@@ -33,4 +39,8 @@ app.listen(8080, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
 })
